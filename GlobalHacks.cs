59ccda1c61b662e11081f7681833437a8bad8784
0gjimenez <42749723+0gjimenez@users.noreply.github.com>
2020-04-03 16:46:36 -0500

    Add files via upload

---
+++ GlobalHacks.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GlobalHacks: MonoBehaviour
+{
+    public static int HackCount;
+    public GameObject HackDisplay;
+    public int InternalHack;
+
+    void Update()
+    {
+        InternalHack = HackCount;
+        HackDisplay.GetComponent<Text>().text = "Hacks:" + InternalHack;
+
+    }
+}
---
+++ SellInformation.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SellInformation : MonoBehaviour
+{
+
+    public GameObject textBox;
+
+    public void ClickTheButton()
+    {
+        GlobalHacks.HackCount -= 1;
+        GlobalCash.CashCount += 1;
+    }
+}
